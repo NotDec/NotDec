f933f243075cf0efccb7987c79a1d9653053235f
am009 <warrenwjk@qq.com>
2023-09-20 12:55:25 +0000

    frontend: disable bitcast insts.
    Signed-off-by: am009 <warrenwjk@qq.com>

--- src/frontend/wasm/parser-instruction.cpp
+++ src/frontend/wasm/parser-instruction.cpp
@@ -200,7 +200,7 @@ void BlockContext::visitStoreInst(wabt::StoreExpr* expr) {
         break;
     }
     assert(targetType != nullptr);
-    addr = irBuilder.CreateBitCast(addr, PointerType::getUnqual(targetType));
+    // addr = irBuilder.CreateBitCast(addr, PointerType::getUnqual(targetType));
     irBuilder.CreateStore(val, addr);
 }
 
@@ -293,7 +293,7 @@ void BlockContext::visitLoadInst(wabt::LoadExpr* expr) {
     }
     assert(targetType != nullptr);
 
-    addr = irBuilder.CreateBitCast(addr, PointerType::getUnqual(targetType));
+    // addr = irBuilder.CreateBitCast(addr, PointerType::getUnqual(targetType));
     Value* result = irBuilder.CreateLoad(targetType, addr, "loadResult");
     // possible extension
     switch (expr->opcode)
@@ -1713,11 +1713,9 @@ void BlockContext::visitCallIndirectInst(wabt::CallIndirectExpr* expr) {
     Value* arr[2] = { ConstantInt::getNullValue(index->getType()) , index};
     Value* ptr = irBuilder.CreateGEP(table->getValueType(), table, makeArrayRef(arr, 2));
     
-    // ctx.llvmModule.getDataLayout().getABITypeAlign(funcType);
-    // outs() << *cast<GetElementPtrInst>(ptr);
     Value* funcPtr = irBuilder.CreateLoad(ptr->getType()->getPointerElementType(), ptr, "callind_funcptr");
-    // Value* funcPtr = irBuilder.CreateExtractElement(table, index, "callind_funcptr");
-    funcPtr = irBuilder.CreateBitOrPointerCast(funcPtr, PointerType::get(funcType, 0));
+    // if (ctx.baseCtx.opt.recompile)
+    // funcPtr = irBuilder.CreateBitOrPointerCast(funcPtr, PointerType::get(funcType, 0));
     auto callArgsAlloca = (Value**)calloc(sizeof(Value*), paramCount);
     // https://stackoverflow.com/questions/5458204/unsigned-int-reverse-iteration-with-for-loops
     for (wabt::Index i = paramCount; i-- > 0;) {
